eba4f14146f12e39508ab265fe05bb391c133277
Chotipat Poowongthanarat <topz.classic@gmail.com>
2017-08-27 12:20:42 +0700

    initial Scene

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+class EmployeeList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+
+});
+export default EmployeeList;
